c57fc33e52d6e4b63fc6bf2937df37becba797dc
Matheus Brandino <m.v.brandino@gmail.com>
2018-06-19 16:56:32 -0300

    projeto iniciado

---
+++ app/src/main/java/br/com/caelum/twittelumfluxoaula/TweetActivity.java
@@ -0,0 +1,13 @@
+package br.com.caelum.twittelumfluxoaula;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TweetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet);
+    }
+}
