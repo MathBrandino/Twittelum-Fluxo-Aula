fc429360a1c613ba28e50232770b792d43ec5f6a
Matheus Brandino <m.v.brandino@gmail.com>
2018-06-26 13:46:10 -0300

    criando activity para mostrar os tweets usando tablayout e view pager, criando os fragments

---
+++ app/src/main/java/br/com/caelum/twittelumfluxoaula/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.caelum.twittelumfluxoaula.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import br.com.caelum.twittelumfluxoaula.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/br/com/caelum/twittelumfluxoaula/fragment/ListaTweetsFragment.java
@@ -0,0 +1,6 @@
+package br.com.caelum.twittelumfluxoaula.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class ListaTweetsFragment extends Fragment {
+}
