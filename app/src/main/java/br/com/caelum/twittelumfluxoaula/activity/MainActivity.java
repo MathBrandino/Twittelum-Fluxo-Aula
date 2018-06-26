6196da632a4bffc26be6f52b19e7f391beae2207
Matheus Brandino <m.v.brandino@gmail.com>
2018-06-26 14:03:23 -0300

    criando layout para o fragment de lista, colocando os fragments na tela atraves do adapter

--- app/src/main/java/br/com/caelum/twittelumfluxoaula/activity/MainActivity.java
+++ app/src/main/java/br/com/caelum/twittelumfluxoaula/activity/MainActivity.java
@@ -1,9 +1,18 @@
 package br.com.caelum.twittelumfluxoaula.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.Arrays;
+import java.util.List;
+
 import br.com.caelum.twittelumfluxoaula.R;
+import br.com.caelum.twittelumfluxoaula.adapter.ViewPagerAdapter;
+import br.com.caelum.twittelumfluxoaula.fragment.BuscaTweetFragment;
+import br.com.caelum.twittelumfluxoaula.fragment.ListaTweetsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +21,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ViewPager pager = findViewById(R.id.main_view_pager);
+
+        FragmentManager manager = getSupportFragmentManager();
+        List<Fragment> fragments = Arrays.asList(new ListaTweetsFragment(), new BuscaTweetFragment());
+        ViewPagerAdapter adapter = new ViewPagerAdapter(manager, fragments);
+        pager.setAdapter(adapter);
     }
 }
--- app/src/main/java/br/com/caelum/twittelumfluxoaula/fragment/ListaTweetsFragment.java
+++ app/src/main/java/br/com/caelum/twittelumfluxoaula/fragment/ListaTweetsFragment.java
@@ -1,6 +1,29 @@
 package br.com.caelum.twittelumfluxoaula.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.caelum.twittelumfluxoaula.R;
 
 public class ListaTweetsFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(R.layout.fragment_lista_tweets, container, false);
+
+
+        return view;
+
+    }
+
+
 }
